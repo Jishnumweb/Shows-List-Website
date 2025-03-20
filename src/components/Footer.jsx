a0f5a0715f7619b8bfe736f264a98220dae329c6
jishnum <jishnuum2000@gmail.com>
2025-03-20 13:04:42 +0530

    updated links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ function Footer() {
       <div className="footer text-center">
 
         <div className="footersub gap-2">
-          <a href="https://www.instagram.com/jishnu?igsh=MTA5cG5jZzkxO=="><LuInstagram /></a>
-          <a href="https://x.com/quonsah?t=J9NEgcXjm-KTaAJ0B_uv"><FaXTwitter /></a>
-          <a href="https://www.linkedin.com/in/jisha9700a324?Apage%3Ad_flagship3_profile_view_base_contact_details%3B4kJ5ugxUTnKK92009chomw%3D%3D"><FaLinkedin /></a>
+          <a href=""><LuInstagram /></a>
+          <a href=""><FaXTwitter /></a>
+          <a href=""><FaLinkedin /></a>
           
         </div>
         <p className='ppfoot m-0'>© 2025 Quonsah. All Rights Reserved. | Privacy Policy | Terms & Conditions | Contact Us</p>
